6472f2667980dfcc86bfe08286ebd11ddf5f0a3a
muhammad962-fiv <135445380+muhammad962-fiv@users.noreply.github.com>
2026-05-19 13:02:32 +0500

    Refactor ML_Services.py to improve structure and CORS

--- ML_Services.py
+++ ML_Services.py
@@ -17,7 +17,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # or "*"
+    allow_origins=["http://localhost:3000"], ["https://kratobot-production.up.railway.app"] # or "*"
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
